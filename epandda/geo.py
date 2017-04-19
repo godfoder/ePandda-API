85a67c3f3d603cdd661ea792b3a3eccb7fae7442
Jon Lauters <jonathan.lauters@gmail.com>
2017-04-19 16:32:35 -0500

    Added rough (static) documentation response format

--- epandda/geo.py
+++ epandda/geo.py
@@ -8,16 +8,33 @@ class geoname(mongoBasedResource):
         locality = self.getRequest().args.get('locality')       # param
         lindex = self.client.test.spIndex                       # Mongodb index for localities
 
-        res = lindex.find({"$text": {"$search": locality}})
+        if locality:
+          res = lindex.find({"$text": {"$search": locality}})
 
-        d = []
-        for i in res:
-            terms = i['original_terms']
-            terms.append(locality)
-            item = {"terms": terms, "matches": {"pbdb": i['pbdb_data'], "idigbio": i['idb_data']}}
-            d.append(item)
+          d = []
+          for i in res:
+              terms = i['original_terms']
+              terms.append(locality)
+              item = {"terms": terms, "matches": {"pbdb": i['pbdb_data'], "idigbio": i['idb_data']}}
+              d.append(item)
 
-        d = self.resolveReferences(d)
-        resp = self.toJson(d)
+          d = self.resolveReferences(d)
+          resp = self.toJson(d)
+
+        else:
+
+          # TODO: Think how best to automate / make dynamic
+          resp = {'success': {
+                      'v': 1,
+                      'description': 'returns specimens collected from a given locality',
+                      'params': {
+                         'locality': {
+                            'type': 'text',
+                            'required': True,
+                            'description': 'Name of the Locality where returned specimens were collected.'
+                          }
+                      }
+                    }
+                 }
 
         return resp
