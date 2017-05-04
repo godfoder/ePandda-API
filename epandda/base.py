511c59e82dde7bdbdabb73aeccf076f1fb339ac4
mwbenowitz <mwbenowitz@gmail.com>
2017-05-04 17:49:44 -0400

    Geo endpoint to go with localityIndex and pbdb resolver point to API

--- epandda/base.py
+++ epandda/base.py
@@ -17,7 +17,7 @@ class baseResource(Resource):
         # Load API config
         self.config = json.load(open('./config.json'))
 
-        self.client = MongoClient(self.config['mongodb_user'] + ":" + self.config['mongodb_password'] + "@" + self.config['mongodb_host'])
+        self.client = MongoClient("mongodb://" + self.config['mongodb_user'] + ":" + self.config['mongodb_password'] + "@" + self.config['mongodb_host'])
         self.idigbio = self.client.idigbio.occurrence
         self.pbdb = self.client.test.pbdb_flat_index
 
@@ -38,7 +38,7 @@ class baseResource(Resource):
 
             resolved = []
             for mitem in m:
-                resolved.append({"uuid": mitem['idigbio:uuid']})
+                resolved.append({"uuid": mitem['idigbio:uuid'], "url": "https://www.idigbio.org/portal/records/" + mitem['idigbio:uuid']})
             resolved_references["idigbio_resolved"] = resolved
 
             # resolve pbdb refs
@@ -47,7 +47,7 @@ class baseResource(Resource):
 
             resolved = []
             for mitem in m:
-                resolved.append({"occurrence_no": mitem['occurrence_no']})
+                resolved.append('https://paleobiodb.org/data1.2/occs/single.json?id=' + mitem['occurrence_no'] + '&show=full')
             resolved_references["pbdb_resolved"] = resolved
 
         return resolved_references
--- epandda/geo.py
+++ epandda/geo.py
@@ -1,7 +1,10 @@
 from mongo import mongoBasedResource
 from response_handler import response_handler
 from flask_restful import reqparse
-
+import datetime
+import gridfs
+import json
+from bson import ObjectId
 parser = reqparse.RequestParser()
 
 parser.add_argument('locality', type=str, help='Locality name to to search geoname records by')
@@ -11,21 +14,162 @@ parser.add_argument('locality', type=str, help='Locality name to to search geona
 #
 class geoname(mongoBasedResource):
     def get(self):
-        locality = self.getRequest().args.get('locality')       # param
-        lindex = self.client.test.spIndex                       # Mongodb index for localities
+        # Mongodb index for localities
+        lindex = self.client.test.localityIndex   
 
-        if locality:
-          res = lindex.find({"$text": {"$search": locality}})
+        # Mongodb gridFS instance
+        grid = gridfs.GridFS(self.client.test)                   
+
+        # Parameters
+        # required
+        # NONE
+
+        # optional (at least one is necessary)
+        geolocation = self.getRequest().args.get('geolocation')
+        countryName = self.getRequest().args.get('countryName')
+        countryCode = self.getRequest().args.get('countryCode')
+        stateProvinceName = self.getRequest().args.get('stateProvinceName')
+        stateProvinceCode = self.getRequest().args.get('stateProvinceCode')
+        county = self.getRequest().args.get('county')
+        locality = self.getRequest().args.get('locality')
+        geoPoint = self.getRequest().args.get('geoPoint')
+        geoUncertainty = self.getRequest().args.get('geoUncertainty')
+
+        offset = self.getRequest().args.get('offset')
+        limit = self.getRequest().args.get('limit') 
+
+        params = [
+          {
+            "name": "geolocation",
+            "type": "text",
+            "value": geolocation,
+            "required": False,
+            "description": "A generic place name, will search across locality, county, state/province and country fields"
+          },
+          {
+            "name": "countryName",
+            "type": "text",
+            "value": countryName,
+            "required": False,
+            "description": "Full country names"
+          },
+          {
+            "name": "countryCode",
+            "type": "text",
+            "value": countryCode,
+            "required": False,
+            "description": "2-letter country code, provided from geonames.org"
+          },
+          {
+            "name": "stateProvince",
+            "type": "text",
+            "value": stateProvinceName,
+            "required": False,
+            "description": "State or province name. Do not include qualifiers (e.g. State, Province) to return better results"
+          },
+          {
+            "name": "stateProvinceCode",
+            "type": "text",
+            "value": stateProvinceCode,
+            "required": False,
+            "description": "State or province name. Do not include qualifiers (e.g. State, Province) to return better results"
+          },
+          {
+            "name": "county",
+            "type": "text",
+            "value": county,
+            "required": False,
+            "description": 'County name. Do not add "County" to your search string'
+          },
+          {
+            "name": "locality",
+            "type": "text",
+            "value": locality,
+            "required": False,
+            "description": "The specific locality where a specimen was recorded"
+          },
+          {
+            "name": "geoPoint",
+            "type": "text",
+            "value": geoPoint,
+            "required": False,
+            "description": "The Latitude, Longitude to search on. Provide a comma separated string"
+          },
+          {
+            "name": "geoUncertainty",
+            "type": "text",
+            "value": geoUncertainty,
+            "required": False,
+            "description": "The distance from the provided geoPoint to match specimens within. The distance provided will be used to draw a circle around the geoPoint"
+          }
+        ]
+
+        if len(self.getRequest().args) > 0:
+          criteria = {'endpoint': 'geoname', 'parameters': {}, 'matchTerms': {'stateProvinceNames': [], 'countryNames': [], 'countyNames': [], 'originalStates': [], 'originalCountries': [], 'originalCounties': []}}
+          geoQuery = []
+          if locality:
+            criteria['parameters']['locality'] = locality
+            geoQuery.append({"$text": {"$search": locality}})
+          if stateProvinceName:
+            criteria['parameters']['stateProvinceName'] = stateProvinceName
+            geoQuery.append({'stateProvinceName': stateProvinceName})
+          if countryName:
+            criteria['parameters']['countryName'] = countryName
+            geoQuery.append({'countryName': countryName})
+          if countryCode:
+            criteria['parameters']['countryCode'] = countryCode
+            geoQuery.append({'countryCode': countryCode})
+          if county:
+            criteria['parameters']['county'] = county
+            geoQuery.append({'county': county})
+          if not limit:
+              limit = None
+          if not offset:
+              offset = 0
+          #res = lindex.find({"$text": {"$search": locality}})
+          res = lindex.find({'$and': geoQuery})
 
           d = []
+          matches = {'idigbio': [], 'pbdb': []}
+          total_count = 0
           for i in res:
-              terms = i['original_terms']
-              terms.append(locality)
-              item = {"terms": terms, "matches": {"pbdb": i['pbdb_data'], "idigbio": i['idb_data']}}
-              d.append(item)
+            if i['countryName'] not in criteria['matchTerms']['countryNames']:
+              criteria['matchTerms']['countryNames'].append(i['countryName'])
+            if i['stateProvinceName'] not in criteria['matchTerms']['stateProvinceNames']:
+              criteria['matchTerms']['stateProvinceNames'].append(i['stateProvinceName'])
+            if i['county'] not in criteria['matchTerms']['countyNames']:
+              criteria['matchTerms']['countyNames'].append(i['county'])
+            for origState in i['originalStateProvinceName']:
+              if origState not in criteria['matchTerms']['originalStates']:
+                criteria['matchTerms']['originalStates'].append(origState)
+            for origCountry in i['originalCountryName']:
+              if origCountry not in criteria['matchTerms']['originalCountries']:
+                criteria['matchTerms']['originalCountries'].append(origCountry)
+            for origCounty in i['original_county']:
+              if origCounty not in criteria['matchTerms']['originalCounties']:
+                criteria['matchTerms']['originalCounties'].append(origCounty)
+            gridInstance = i['pbdbGridFile']
+            grid_doc = grid.get(gridInstance)
+            grid_matches = json.loads(grid_doc.read())
+            total_count += len(grid_matches)
+            matches['pbdb'] = matches['pbdb'] + grid_matches
+              #terms = i['original_terms']
+              #terms.append(locality)
+              #item = {"terms": terms, "matches": {"pbdb": i['pbdb_data'], "idigbio": i['idb_data']}}
+              #d.append(item)
 
+          item = {'matches': matches}
+          d.append(item)
           d = self.resolveReferences(d)
-          resp = self.toJson(d)
+          #idb_count = len(d['idigbio_resolved'])
+          #pbdb_count = len(d['pbdb_resolved'])
+          #counts = {'totalCount': idb_count + pbdb_count, 'idbCount': idb_count, 'pbdbCount': pbdb_count}
+
+          
+          d['pbdb_resolved'] = d['pbdb_resolved'][offset:limit]
+          #resp = self.toJson({'totalCount': counts, 'timeReturned':  str(datetime.datetime.now()), 'limit': limit, 'offset': offset, 'specimenData': False, 'includeAnnotations': False, 'results': grid_doc, 'criteria': criteria})
+          resp = self.toJson({'totalCount': total_count, 'timeReturned':  str(datetime.datetime.now()), 'limit': limit, 'offset': offset, 'specimenData': False, 'includeAnnotations': False, 'results': d, 'criteria': criteria})
+          return resp
 
         else:
 
@@ -36,7 +180,7 @@ class geoname(mongoBasedResource):
           
 
 
-        return response_handler( resp )
+        return resp
 
     def post(self):
       args = parser.parse_args()
@@ -46,4 +190,4 @@ class geoname(mongoBasedResource):
         'params': args
       }
 
-      return response_handler( resp )
+      return resp 
