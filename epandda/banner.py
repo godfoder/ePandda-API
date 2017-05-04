cdd3e88111181c1287cde1b75369554ebdc1a640
Seth <seth@collectiveaccess.org>
2017-05-04 14:10:04 -0400

    Omit static routh

--- epandda/banner.py
+++ epandda/banner.py
@@ -10,17 +10,19 @@ class show(Resource):
 
         routes = []
         for rule in current_app.url_map.iter_rules():
+          if (rule.endpoint == 'static'):
+              continue
 
           options = {}
           for arg in rule.arguments:
             options[arg] = "[{0}]".format(arg)
 
           url = url_for(rule.endpoint, **options)
-          routes.append({'url': url, 'methods': ",".join(rule.methods) }) 
+          routes.append({'url': url, 'methods': ",".join(rule.methods) })
 
         resp = {
           'endpoint_description': 'ePANDDA REST API guide',
-          'routes': routes
+          'routes': routes,
         }
 
         return response_handler( resp )
