17395cb2bcdc76f67ba3693bce74be0ce4101596
Seth <seth@collectiveaccess.org>
2017-05-08 13:56:04 -0400

    Add Mongo db and collection

--- epandda/sources/idigbio.py
+++ epandda/sources/idigbio.py
@@ -1,16 +1,33 @@
 class idigbio:
-    def availableFields:
-     return ["id", "idigbio:barcodeValue", "dwc:basisOfRecord", "dwc:bed", "dwc:catalogNumber",
-     "dwc:class", "dwc:collectionCode", "dwc:collectionID", "idigbio:collectionName", "dwc:recordedBy",
-     "dwc:vernacularName", "dwc:continent", "dwc:coordinateUncertaintyInMeters", "dwc:country", "idigbio:isoCountryCode",
-     "dwc:county", "dwc:eventDate", "idigbio:dateModified", "idigbio:dataQualityScore", "dwc:earliestAgeOrLowestStage",
-     "dwc:earliestEpochOrLowestSeries", "dwc:earliestEraOrLowestErathem", "dwc:earliestPeriodOrLowestSystem", "idigbio:etag",
-     "dwc:family", "dwc:fieldNumber", "idigbio:flags", "dwc:formation", "gbif:cannonicalName", "gbif:genus", "gbif:specificEpithet",
-     "gbif:taxonID", "dwc:genus", "idigbio:geoPoint", "idigbio:geoShape", "dwc:group", "idigbio:hasImage", "idigbio:hasMedia",
-     "dwc:higherClassification", "dwc:individualCount", "dwc:infraspecificEpithet", "dwc:institutionCode", "dwc:institutionID",
-     "idigbio:institutionName", "dwc:kingdom", "dwc:latestAgeOrHighestStage", "dwc:latestEpochOrHighestSeries", "dwc:latestEraOrHighestErathem",
-     "dwc:latestPeriodOrHighestSystem", "dwc:lithostratigraphicTerms", "dwc:locality", "dwc:lowestBiostratigraphicZone", "dwc:maximumDepthInMeters",
-     "dwc:maximumElevationInMeters", "idigbio:mediarecords", "dwc:member", "dwc:minimumDepthInMeters", "dwc:minimumElevationInMeters",
-     "dwc:municipality", "dwc:occurrenceID", "dwc:order", "dwc:phylum", "idigbio:recordIds", "dwc:recordNumber", "idigbio:recordset",
-     "dwc:scientificName", "dwc:specificEpithet", "dwc:startDayOfYear", "dwc:stateProvince", "dwc:typeStatus", "idigbio:uuid",
-     "dwc:verbatimEventDate", "dwc:verbatimLocality", "idigbio:version", "dwc:waterBody"]
+        def dbName(self):
+                return "idigbio"
+
+        def collectionName(self):
+                return "occurrence"
+
+        def availableFields(self):
+                return ["id", "idigbio:barcodeValue", "dwc:basisOfRecord", "dwc:bed", "dwc:catalogNumber",
+                        "dwc:class", "dwc:collectionCode", "dwc:collectionID", "idigbio:collectionName", "dwc:recordedBy",
+                        "dwc:vernacularName", "dwc:continent", "dwc:coordinateUncertaintyInMeters", "dwc:country",
+                        "idigbio:isoCountryCode",
+                        "dwc:county", "dwc:eventDate", "idigbio:dateModified", "idigbio:dataQualityScore",
+                        "dwc:earliestAgeOrLowestStage",
+                        "dwc:earliestEpochOrLowestSeries", "dwc:earliestEraOrLowestErathem", "dwc:earliestPeriodOrLowestSystem",
+                        "idigbio:etag",
+                        "dwc:family", "dwc:fieldNumber", "idigbio:flags", "dwc:formation", "gbif:cannonicalName", "gbif:genus",
+                        "gbif:specificEpithet",
+                        "gbif:taxonID", "dwc:genus", "idigbio:geoPoint", "idigbio:geoShape", "dwc:group", "idigbio:hasImage",
+                        "idigbio:hasMedia",
+                        "dwc:higherClassification", "dwc:individualCount", "dwc:infraspecificEpithet", "dwc:institutionCode",
+                        "dwc:institutionID",
+                        "idigbio:institutionName", "dwc:kingdom", "dwc:latestAgeOrHighestStage",
+                        "dwc:latestEpochOrHighestSeries", "dwc:latestEraOrHighestErathem",
+                        "dwc:latestPeriodOrHighestSystem", "dwc:lithostratigraphicTerms", "dwc:locality",
+                        "dwc:lowestBiostratigraphicZone", "dwc:maximumDepthInMeters",
+                        "dwc:maximumElevationInMeters", "idigbio:mediarecords", "dwc:member", "dwc:minimumDepthInMeters",
+                        "dwc:minimumElevationInMeters",
+                        "dwc:municipality", "dwc:occurrenceID", "dwc:order", "dwc:phylum", "idigbio:recordIds",
+                        "dwc:recordNumber", "idigbio:recordset",
+                        "dwc:scientificName", "dwc:specificEpithet", "dwc:startDayOfYear", "dwc:stateProvince",
+                        "dwc:typeStatus", "idigbio:uuid",
+                        "dwc:verbatimEventDate", "dwc:verbatimLocality", "idigbio:version", "dwc:waterBody"]
--- epandda/sources/paleobio.py
+++ epandda/sources/paleobio.py
@@ -1,8 +1,18 @@
 class pbdb:
-    def availableFields:
-        return ["interval_no","paleolat","species_name","author1last","collectors","occurrence_no","reference_no",
-        "interval_name","county","taxon_name","collection_type","common_name","type_specimen","lng","otherauthors","period_max",
-        "subgenus_name","author2init","pubno","museum","state","comments","member","eml_interval","reftitle","taxon_rank","abund_unit",
-        "paleolng","collection_aka","epoch_min","epoch_max","pubtitle","genus_name","formation","lat","abund_value","period_min","doi",
-        "catalog_number","collection_no","collection_name","pubyr","author1init","intage_max","taxon_no","country","author2last","pubvol",
-        "intage_min"]
+    def dbName(self):
+        return "test"
+
+    def collectionName(self):
+        return "pbdb_flat_index"
+
+    def availableFields(self):
+        return ["interval_no", "paleolat", "species_name", "author1last", "collectors", "occurrence_no", "reference_no",
+                "interval_name", "county", "taxon_name", "collection_type", "common_name", "type_specimen", "lng",
+                "otherauthors", "period_max",
+                "subgenus_name", "author2init", "pubno", "museum", "state", "comments", "member", "eml_interval",
+                "reftitle", "taxon_rank", "abund_unit",
+                "paleolng", "collection_aka", "epoch_min", "epoch_max", "pubtitle", "genus_name", "formation", "lat",
+                "abund_value", "period_min", "doi",
+                "catalog_number", "collection_no", "collection_name", "pubyr", "author1init", "intage_max", "taxon_no",
+                "country", "author2last", "pubvol",
+                "intage_min"]
